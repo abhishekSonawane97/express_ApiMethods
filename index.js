c18c4a25f14edfe04abddc0a4341b60f33b3457f
Abhishek Sonawane <109890906+abhishekSonawane97@users.noreply.github.com>
2023-03-17 22:44:30 +0530

    routing
    different files for different endpoints

--- index.js
+++ index.js
@@ -1,57 +1,24 @@
 const express = require('express');
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+const calculatorRoutes = require('./routes/calculatorRoute')
+const todoRoutes = require('./routes/todoRoute')
 
 const port = 2000;
 
 
 app.get('/', (req, res)=>{
-    res.send('Digikull')
+    res.send(`<h1>Digikull</h1>`)
 });
 
 
-let x = 100;
 
-app.get('/calculator', (req, res)=>{
-    res.send(`value : ${x}`)
-});
+let file = `<h1>Home Page</h1>`;
 
-app.put('/calculator', (req, res)=>{
-    let newVal = x+(req.body.val);
-    res.send(`value : ${newVal}`)
-});
-
-app.delete('/calculator', (req, res)=>{
-    res.send(`value has been reset : ${x}`)
-});
-
-
-let arr = [11, 22, 33, 44, 55, 66]
-app.get('/todos', (req, res)=>{
-    res.send(`Array Items : ${arr}`)
-});
-
-app.post('/todos', (req, res)=>{
-    let newVal = (req.body.val)
-    let newArr = arr
-    newArr.push(newVal)
-    arr=  [...newArr];
-    res.send(`New item has beed added, New Array Items : ${arr}`)
-});
-
-
-app.delete('/todos/:ind', (req, res)=>{
-    let indexNum = Number(req.params.ind);
-    let newArr = arr 
-    for(let i=0;i<newArr.length;i++){
-        if(indexNum === i){
-            newArr.splice(i,1)
-            break;
-        }
-    }
-    arr=  [...newArr];
-    res.send(`value has been deleted, New Array Items : ${arr}`)
-});
+app.use('/calculator', calculatorRoutes)
+app.use('/todos', todoRoutes)
+// app.use('/home', homeRoute)
+// app.use('/about', homeRoute)
 
 app.listen(port, ()=> console.log('server is running on port 2000'))
