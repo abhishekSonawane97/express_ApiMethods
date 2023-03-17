9a852726826a89cc24838a26bad7ba0d1ad0d3b4
Abhishek Sonawane <109890906+abhishekSonawane97@users.noreply.github.com>
2023-03-17 22:47:54 +0530

    routing endpoints

--- index.js
+++ index.js
@@ -7,18 +7,11 @@ const todoRoutes = require('./routes/todoRoute')
 
 const port = 2000;
 
-
 app.get('/', (req, res)=>{
-    res.send(`<h1>Digikull</h1>`)
+    res.send(`<h1 style='text-align:center; margin-top: 20vh; color:cyan; background: black;'>Digikull</h1>`)
 });
-
-
-
-let file = `<h1>Home Page</h1>`;
 
 app.use('/calculator', calculatorRoutes)
 app.use('/todos', todoRoutes)
-// app.use('/home', homeRoute)
-// app.use('/about', homeRoute)
 
 app.listen(port, ()=> console.log('server is running on port 2000'))
