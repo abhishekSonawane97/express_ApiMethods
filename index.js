5ace0e952aac015c08bcb3300d7cd07618c10989
Abhishek Sonawane <109890906+abhishekSonawane97@users.noreply.github.com>
2023-03-18 14:02:34 +0530

    jwt authentication

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
-const express = require('express');
-
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
-const calculatorRoutes = require('./routes/calculatorRoute')
-const todoRoutes = require('./routes/todoRoute')
+const calculatorRoutes = require("./routes/calculatorRoute");
+const todoRoutes = require("./routes/todoRoute");
 
 const port = 2000;
+const secreteKey = "shoooo";
 
-app.get('/', (req, res)=>{
-    res.send(`<h1 style='text-align:center; margin-top: 20vh; color:cyan; background: black;'>Digikull</h1>`)
+app.get("/", (req, res) => {
+  res.send(
+    `<h1 style='text-align:center; margin-top: 20vh; color:cyan; background: black;'>Digikull</h1>`
+  );
 });
 
-app.use('/calculator', calculatorRoutes)
-app.use('/todos', todoRoutes)
+app.use("/calculator", calculatorRoutes);
+app.use("/todos", todoRoutes);
 
-app.listen(port, ()=> console.log('server is running on port 2000'))
+app.post("/login", (req, res) => {
+  const user = {
+    id: 1,
+    name: "user1",
+    email: "user1@gmail.com",
+  };
+  jwt.sign({ user }, secreteKey, { expiresIn: "400s" }, (err, token) => {
+    res.json({ token });
+  });
+});
+
+const verifyToken = (req, res, next) => {
+  const bearerHeader = req.headers["authentication"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const token = bearer[1];
+    // console.log(bearer, token)
+    req.token = token;
+    next();
+  } else {
+    res.send("invalid token");
+  }
+};
+
+app.post("/home", verifyToken, (req, res) => {
+  jwt.verify(req.token, secreteKey, (err, auth) => {
+    if (err) {
+      res.status(400).json({
+        message: err.message,
+      });
+    } else {
+      res.status(200).json({
+        message: "welcome to home page",
+        user: auth["user"],
+      });
+    }
+  });
+});
+
+app.listen(port, () => console.log("server is running on port 2000"));
